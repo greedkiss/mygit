10c28a9f000448795b2186f68010d7a8342d183c
flipped <greedkiss@163.com>
2020-07-14 10:11:34 +0800

    [+] you can run shell in the terminal

--- mygit.js
+++ mygit.js
@@ -3,21 +3,29 @@
 
 var fs = require("fs");
 var nodepath = require("path");
-const { POINT_CONVERSION_UNCOMPRESSED } = require("constants");
+const { throws } = require("assert");
 
 
 var mygit = module.exports = {
     init: function(opts){
-        fs.mkdirSync('letme', 777);
-        console.log(fs.existsSync());
+        console.log("haahah");
 
     }
     
 }
 
+//处理脚本参数，opts如下
+// {
+//   _: [
+//     '/usr/local/bin/node',
+//     '/home/flipped/project/my_git/mygit.js',
+//     'init'
+//   ],
+//   a: true
+// }
 var parseOptions = function(argv){
     var name;
-    return argc.reduce(function(opts, arg) {
+    return argv.reduce(function(opts, arg) {
         if(arg.match(/^-/)){
             name = arg.replace(/^-+/, "");
             opts[name] = true;
@@ -27,14 +35,41 @@ var parseOptions = function(argv){
         }else{
             opts._.push(arg);
         }
-
         return opts;
-    }, {});
+    }, { _ : []});
 };
 
+var  runCli = function(argv){
+    var opts = parseOptions(argv);
+    var commandName = opts._[2];
+    
+    if(commandName === undefined){
+        throw new Error("you must specify a command to run the code!");
+    }else{
+        var commandFnName = commandName.replace(/-/g,"_");
+        var fn = mygit[commandFnName];
+
+        if(fn === undefined){
+            throw new Error("the function is not defined");
+        }else{
+            var commandArgs = opts._.slice(3);
+            //function.length 代表形参个数
+            while (commandArgs.length < fn.length - 1){
+                commandArgs.push(undefined);
+            }
+
+            return fn.apply(mygit, commandArgs.concat(opts));
+        }
+    }
+}
 
 if(require.main === module){
-
-
-
+    try{
+        var result = runCli(process.argv);
+        if(result　!== undefined){
+            console.log(result);
+        }
+    }catch(e){
+        console.error(e.toString());
+    }
 }
