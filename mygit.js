d7b85f28a568d5766eb28560ffcf660b736b2085
flipped <greedkiss@163.com>
2020-07-21 22:04:18 +0800

    :tada: status

--- mygit.js
+++ mygit.js
@@ -280,9 +280,14 @@ var mygit = module.exports = {
                             "Count " + objects.allObjects().length,
                             branch + " -> " + branch].join("\n") + "\n";
                 }
-
             }
         }
+    },
+
+    status: function(){
+        files.assertInRepo();
+        config.assertNotBare();
+        return status.toString();
     },
 
     write_tree: function(_){
@@ -860,6 +865,49 @@ var diff = {
     }
 }
 
+var status = {
+
+    toString: function() {
+  
+      // **untracked()** returns an array of lines listing the files not
+      // being tracked by Gitlet.
+      function untracked() {
+        return fs.readdirSync(files.workingCopyPath())
+            .filter(function(p) { return index.toc()[p] === undefined && p !== ".gitlet"; });
+      };
+  
+      // **toBeCommitted()** returns an array of lines listing the files
+      // that have changes that will be included in the next commit.
+      function toBeCommitted() {
+        var headHash = refs.hash("HEAD");
+        var headToc = headHash === undefined ? {} : objects.commitToc(headHash);
+        var ns = diff.nameStatus(diff.tocDiff(headToc, index.toc()));
+        return Object.keys(ns).map(function(p) { return ns[p] + " " + p; });
+      };
+  
+      // **notStagedForCommit()** returns an array of lines listing the
+      // files that have changes that will not be included in the next
+      // commit.
+      function notStagedForCommit() {
+        var ns = diff.nameStatus(diff.diff());
+        return Object.keys(ns).map(function(p) { return ns[p] + " " + p; });
+      };
+  
+      // **listing()** keeps `lines` (prefixed by `heading`) only if it's nonempty.
+      function listing(heading, lines) {
+        return lines.length > 0 ? [heading, lines] : [];
+      }
+  
+      // Gather all the sections, keeping only nonempty ones, and flatten them
+      // together into a string.
+      return util.flatten(["On branch " + refs.headBranchName(),
+                           listing("Untracked files:", untracked()),
+                           listing("Unmerged paths:", index.conflictedPaths()),
+                           listing("Changes to be committed:", toBeCommitted()),
+                           listing("Changes not staged for commit:", notStagedForCommit())])
+          .join("\n");
+    }
+  };
 
 var refs = {
     commitParentHashes: function() {
